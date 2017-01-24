5eabde4f1f1aa37f7664cfe3b7e401dec400d9ef
mrvivacious <v.bhookya98@gmail.com>
2017-01-23 21:37:09 -0600

    Solved(?)
    Modified to program to work properly — as far as I can tell.
    Added jUnit tests as well.

--- src/wordBuddies_vbhook2/Main.java
+++ src/wordBuddies_vbhook2/Main.java
@@ -8,52 +8,66 @@ package wordBuddies_vbhook2;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.print(isWordBuddies(args));
+    }
+
+    public static String isWordBuddies(String[] args) {
         String[] substrings = new String[args.length];
-        int counter = 0;
-        boolean multiple = false;
+        String buddies = "";
+        boolean foundBuddy = false;
         boolean noBuddy = false;
+        int counter = 0;
 
         //initialization
         for (int i = 0; i < substrings.length; i++) {
-            substrings[i] = "none";
+            substrings[i] = "";
         }
         //select initial word
         for (int i = 0; i < args.length; i++) {
             //select word to test for WordBuddies
                 for (int j = 0; j < args.length; j++) {
-                    if (i != j ) {
-                        //substring selection process for initial word
-                        for (int k = 0; k < args[i].length() - 1; k++) {
-                            //substring selection process for test word
-                            for (int l = 0; l < args[j].length() - 1; l++) {
-                                if (args[i].substring(k, k + 2).equalsIgnoreCase(args[j].substring(l, l + 2))) {
-                                    counter++;
-                                    substrings[i] = args[i].substring(k, k + 2);
+                    if ( i != j ) {
+                        //change this to iterate every index
+                        for (int k = 0; k < args[i].length(); k++) {
+                            //change this to iterate every index of test word
+                            for (int l = 0; l < args[j].length(); l++) {
+                                if ( args[i].substring(k, k + 1).equalsIgnoreCase(args[j].substring(l, l + 1) ) &&
+                                        (k == l) && foundBuddy) {
+                                    counter++;                            //"buddy" qualifies with x > 2 matches, one matching character
+                                    if( counter >= 2 ) {                  //should not be treated as a buddy and therefore not print "Not Buddies"
+                                        return "Not Buddies";
+                                    }
                                 }
-
+                                else if( args[i].substring(k, k + 1).equalsIgnoreCase(args[j].substring(l, l + 1)) &&
+                                        (k == l) ) {
+                                    substrings[i] += args[i].substring(k, k + 1).toLowerCase();
+                                }
                             }
-
                         }
-
                     }
+                    //if buddies have been found, ensure no additional words affect the substring array's items
+                    if ( !substrings[i].equals("") && substrings[i].length() >= 2 ) {
+                        foundBuddy = true;
+                    }
+                    else
+                        substrings[i] = "";
                 }
-            //does one word have multiple buddies?
-            if( counter > 1 ) {
-                multiple = true;
-            }
             //reset counter for next word
+            foundBuddy = false;
             counter = 0;
         }
         //does everyone have a buddy?
         for (int i = 0; i < substrings.length; i++) {
-            if(substrings[i].equals("none"))
+            if( substrings[i].equals("") )
                 noBuddy = true;
         }
-        if( multiple || noBuddy)
-            System.out.print("Not Buddies");
-        else
+        if( noBuddy )
+            return "Not Buddies";
+        else {
             for (int i = 0; i < substrings.length; i++) {
-                System.out.print(substrings[i] + " ");
+                buddies += substrings[i] + " ";
             }
+            return buddies.substring(0, buddies.length() - 1);
+        }
     }
 }
---
+++ test/wordBuddies_vbhook2/MainTest.java
@@ -0,0 +1,40 @@
+package wordBuddies_vbhook2;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by vbhook2 on 1/23/17.
+ */
+public class MainTest {
+    @Test
+    public void buddies() throws Exception {
+        String[] goodInput = {"scar", "feature", "fee", "tsarina"};
+        assertEquals("ar fe fe ar", Main.isWordBuddies(goodInput));
+
+        String[] greatInput = {"SCAT", "tHaT", "mOuSe", "HoUSE"};
+        assertEquals("at at ouse ouse", Main.isWordBuddies(greatInput));
+    }
+
+    @Test
+    public void notBuddies() throws Exception {
+        String[] badInput = {"scar", "feature", "fee", "fear"};
+        assertEquals("Not Buddies", Main.isWordBuddies(badInput));
+
+        String[] notGreatInput = {"scat", "that", "mouse"};
+        assertEquals("Not Buddies", Main.isWordBuddies(notGreatInput));
+    }
+
+    @Test
+    public void otherInputs() throws Exception {
+        String[] usedAndAbusedInput = {"1", "2", "3", "2", "1", "3"};
+        assertEquals( "Not Buddies", Main.isWordBuddies(usedAndAbusedInput));
+
+        String[] noInput = {""};
+        assertEquals("Not Buddies", Main.isWordBuddies(noInput));
+
+        String[] nullInput = {null};
+        assertEquals("Not Buddies", Main.isWordBuddies(nullInput));
+    }
+}
