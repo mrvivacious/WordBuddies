6656b5a6f0906f707d480b10723fc96a5bd794f0
mrvivacious <v.bhookya98@gmail.com>
2017-01-31 13:28:01 -0600

    Added helper functions

--- src/wordBuddies_vbhook2/Main.java
+++ src/wordBuddies_vbhook2/Main.java
@@ -8,20 +8,22 @@ package wordBuddies_vbhook2;
 public class Main {
 
     public static void main(String[] args) {
-            System.out.print(isWordBuddies(args));
+        System.out.print(isWordBuddies(args));
     }
 
+    /**
+     *This class allows us to run our test cases through its return capabilities
+     * @param args passed in as String array
+     * @return A String array of the buddies, or "Not Buddies" should the args fail
+     */
     public static String isWordBuddies(String[] args) {
         String[] substrings = new String[args.length];
         String buddies = "";
         boolean foundBuddy = false;
-        boolean noBuddy = false;
         int numberOfBuddies = 0;
 
         //initialization
-        for (int i = 0; i < substrings.length; i++) {
-            substrings[i] = "";
-        }
+        initialization(substrings);
         //select initial word
         for (int initialWord = 0; initialWord < args.length; initialWord++) {
             //select word to test for WordBuddies
@@ -31,16 +33,18 @@ public class Main {
                         for (int k = 0; k < args[ initialWord ].length(); k++) {
                             //iterates test word
                             for (int l = 0; l < args[ testWord ].length(); l++) {
-                                if ( args[ initialWord ].substring(k, k + 1).equalsIgnoreCase(args[ testWord ].substring(l, l + 1) ) &&
-                                        (k == l) && foundBuddy) {
+                                String init = args[ initialWord ].substring(k, k+1);
+                                String test = args[ testWord ].substring(l, l + 1);
+                                if ( init.equalsIgnoreCase( test ) &&
+                                        ( k == l ) && foundBuddy ) {
                                     numberOfBuddies++;
                                     if( numberOfBuddies >= 2 ) {
                                         return "Not Buddies";
                                     }
                                 }
-                                else if( args[ initialWord ].substring(k, k + 1).equalsIgnoreCase(args[ testWord ].substring(l, l + 1)) &&
-                                        (k == l) ) {
-                                    substrings[ initialWord ] += args[ initialWord ].substring(k, k + 1).toLowerCase();
+                                else if( init.equalsIgnoreCase( test ) &&
+                                        ( k == l ) ) {
+                                    substrings[ initialWord ] += init.toLowerCase();
                                 }
                             } //testWord iteration
                         } //initialWord iteration
@@ -55,26 +59,46 @@ public class Main {
             foundBuddy = false;
             numberOfBuddies = 0;
         }
-        //does everyone have a buddy?
-        for (int i = 0; i < substrings.length; i++) {
-            if( substrings[i].equals("") )
-                noBuddy = true;
-        }
-        if( noBuddy )
+        if( buddyTest( substrings ) )
             return "Not Buddies";
         else {
-            for (int i = 0; i < substrings.length; i++) {
-                buddies += substrings[i];
-                if( i != substrings.length-1 ) {
+            for ( int index = 0; index < substrings.length; index++ ) {
+                buddies += substrings[ index ];
+                if( index != substrings.length-1 ) {
                     buddies += " ";
                 }
             }
             //If absolutely nothing was passed into the program
-            if( buddies.trim().equals("") ) {
+            if( buddies.trim().equals( "" ) ) {
                 return "Not Buddies";
             }
             else
                 return buddies;
         }
+    }
+
+    /**
+     * Initialize an array of the size of the args
+     * @param substrings
+     * @return
+     */
+    public static String[] initialization(String[] substrings) {
+        for (int i = 0; i < substrings.length; i++) {
+            substrings[i] = "";
+        }
+        return substrings;
+    }
+
+    /**
+     * Checks for any unchanged indexes. This would mean that a part of the arguments has no buddy
+     * @param substrings
+     * @return true if an argument is missing a buddy, false if every argument has a buddy
+     */
+    public static boolean buddyTest(String[] substrings) {
+        for (int i = 0; i < substrings.length; i++) {
+            if( substrings[i].equals( "" ) )
+                return true;
+        }
+        return false;
     }
 }
--- test/wordBuddies_vbhook2/MainTest.java
+++ test/wordBuddies_vbhook2/MainTest.java
@@ -3,20 +3,30 @@ package wordBuddies_vbhook2;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
-
+// ?? Add helper functions. Piazza post
 /**
  * Created by vbhook2 on 1/23/17.
  */
 public class MainTest {
+
+    /**
+     * Test for successful cases
+     * @throws Exception
+     */
     @Test
     public void buddies() throws Exception {
         String[] goodInput = {"scar", "feature", "fee", "tsarina"};
         assertEquals("ar fe fe ar", Main.isWordBuddies(goodInput));
 
-        String[] greatInput = {"SCAT", "tHaT", "mOuSe", "HoUSE"};
-        assertEquals("at at ouse ouse", Main.isWordBuddies(greatInput));
+        //Is this in the parameters? How do I solve for this situation? Works perfect without numbers
+        String[] greatInput = {"S32CAT", "tH32aT", "mOuSe", "HoUSE"};
+        assertEquals("32at 32at ouse ouse", Main.isWordBuddies(greatInput));
     }
 
+    /**
+     * Test for fail cases
+     * @throws Exception
+     */
     @Test
     public void notBuddies() throws Exception {
         String[] badInput = {"scar", "feature", "fee", "fear      "};
@@ -26,26 +36,31 @@ public class MainTest {
         assertEquals("Not Buddies", Main.isWordBuddies(notGreatInput));
     }
 
+    /**
+     * Test for other possible inputs
+     * @throws Exception
+     */
     @Test
     public void otherInputs() throws Exception {
         String[] usedAndAbusedInput = {"1", "2", "3", "2", "1", "3"};
         assertEquals( "Not Buddies", Main.isWordBuddies(usedAndAbusedInput));
 
-        String[] noInput = {""};
+        String[] emptyInput = {""};
+        assertEquals("Not Buddies", Main.isWordBuddies(emptyInput));
+    }
+
+    /**
+     * Test for edge cases
+     * @throws Exception
+     */
+    @Test
+    public void unlikelyInputs() throws Exception {
+        String[] noInput = {};
         assertEquals("Not Buddies", Main.isWordBuddies(noInput));
 
         String[] nullInput = {null};
         assertEquals("Not Buddies", Main.isWordBuddies(nullInput));
-    }
 
-    @Test
-    public void noInput() throws Exception {
-        String[] noInput = {};
-        assertEquals("Not Buddies", Main.isWordBuddies(noInput));
-    }
-
-    @Test
-    public void newLineTest() throws Exception {
         String[] newLine = {"\n"};
         assertEquals("Not Buddies", Main.isWordBuddies(newLine));
     }
